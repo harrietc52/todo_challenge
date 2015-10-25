c25ba3381e436ebf88479f6b06e0e921b3ed7a4b
Harriet Craven <harrietc52@hotmail.co.uk>
2015-10-25 15:28:05 +0000

    protractors working

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,15 +1,27 @@
 describe('ToDoList', function() {
 
+  var inputBox = element(by.model('todoCtrl.todoTitle'))
+  var addButton = element(by.className('btn'))
   var todos = element.all(by.repeater('todo in todoCtrl.todos'))
-  
+
   beforeEach(function() {
     browser.get('http://localhost:8080');
   });
 
-  it('displays a todo', function() {
-    element(by.model('todoCtrl.todoTitle')).sendKeys('Call home');
-    element(by.className('Add ToDo')).click();
-    expect(todos.first().getText()).toEqual('Call home');
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('todo');
+  });
+
+  // it('displays a todo', function() {
+  //   element(by.model('todoCtrl.todoTitle')).sendKeys('Call home');
+  //   element(by.className('Add')).click();
+  //   expect(todos.first().getText()).toEqual('Call home');
+  // });
+
+  it('find todos', function() {
+    inputBox.sendKeys('hello');
+    addButton.click();
+    expect(todos.get(0).getText()).toEqual('hello');
   });
 
 });
